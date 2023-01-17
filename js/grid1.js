f7cf524b2013ba809c622d00d93832ebf9f37f75
menice4 <95630289+menice4@users.noreply.github.com>
2023-01-17 11:14:44 +0000

    Update grid1.js

--- js/grid1.js
+++ js/grid1.js
@@ -1,7 +1,6 @@
 function setup() {
     const  CELL_SIZE = 80;
     createCanvas(800, 800);
-    noStroke();
     let x = 0;
     let y = 0;
     let xVariation =0;
@@ -11,7 +10,8 @@ function setup() {
     let sizes = [CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 3];
     let currentWidth = random(sizes);
     let currentHeight = random(sizes);
-    let colours = [color("#231e39"), color("#c6331b"),color("#e0b411"),color("#e6e2df"),color("#a7a9ab")];
+    let colours = [color("#231e39"), color("#c6331b"),color("#e0b411"),color("#e6e2df"),color("#a7a9ab"),color("#000000")];
+    strokeWeight(5)
     while(y < height){
         while(x < width) {
             fill(random(colours));
@@ -19,8 +19,18 @@ function setup() {
             // CELL_SIZE + random(-widthVariation , widthVariation ), CELL_SIZE + random(-heightVariation, heightVariation));
             rect(x, y, currentWidth, currentHeight);
             x+= currentWidth;
+            let availableWidth = width - x;
+            currentWidth = random(sizes);
+            while (currentWidth > availableWidth){
+                currentWidth -= CELL_SIZE;
+            }
         }
         y+=currentHeight;
+        let availableHeight = height - y;
+        currentHeight = random(sizes);
+        while(currentHeight > availableHeight){
+            currentHeight -= CELL_SIZE;
+        }
         x=0;
     }
 
