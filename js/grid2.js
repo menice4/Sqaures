9484a9dd6e272ceb85296df4e7441479bafe61fe
menice4 <95630289+menice4@users.noreply.github.com>
2023-01-17 14:46:30 +0000

    sqaures

--- js/grid2.js
+++ js/grid2.js
@@ -5,26 +5,38 @@ function preload(){
         // let textures = loadImage("Images/pattern_"+i+".png"); //concatenation
         let t = loadImage(`Images/pattern_${i}.png`);//backticks are "template literals" and dilllar sign curly brace is interpolation 
         textures.push(t);// adds texture to texture array 
-
-
-
     }
 
 }
 function setup() {
   createCanvas(800, 800);
-  const CELL_SIZE = 80;
+  
+  const CELL_SIZE = 20;
   let x = 0;
   let y = 0;
+  let sizes = [CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 3];
+  let currentWidth = random(sizes);
+  let currentHeight = random(sizes);
+  let colours = [color("#1a4b74"),color("#e2ce81"),color("#9e1d28"),color("#7c9ec1"),color("#ac467a"),color("#e8652e "),color("#3c9c75") ]
   noStroke();
   while(y  < height){
     while (x < width){
-        tint(random(255))
+        tint(random(colours))
         let t = random(textures);
-        image(t, x, y, CELL_SIZE)
-        x+=CELL_SIZE; 
+        image(t, x,y, currentWidth, currentHeight);
+        x+= currentWidth;
+        let availableWidth = width - x;
+        currentWidth = random(sizes);
+        while (currentWidth > availableWidth){
+            currentWidth -= CELL_SIZE;
+        }
     }
-    y+=CELL_SIZE;
+    y+=currentHeight;
+        let availableHeight = height - y;
+        currentHeight = random(sizes);
+        while(currentHeight > availableHeight){
+            currentHeight -= CELL_SIZE;
+        }
     x = 0;
   }
 }
